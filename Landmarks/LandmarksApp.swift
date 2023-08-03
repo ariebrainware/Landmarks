4329b684dacf1ff0252d765cf6b64d6311697cca
Arie Brainware <publicu001@protonmail.com>
2023-08-04 02:50:57 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Arie Brainware on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
