b14b7bd2ecc2c2095ed0ac7dd61e904302f28dd7
Arie Brainware <publicu001@protonmail.com>
2023-08-14 19:15:45 +0700

    Create a Category List

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Arie Brainware on 14/08/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
