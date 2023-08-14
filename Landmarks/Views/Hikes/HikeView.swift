f3fe8bf602c8e0f4803cd90f636cbe135d3f23e1
Arie Brainware <publicu001@protonmail.com>
2023-08-14 16:32:43 +0700

    Animate the Effects of State Changes

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -26,12 +26,15 @@ struct HikeView: View {
                 Spacer()
 
                 Button {
-                    showDetail.toggle()
+                    withAnimation {
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
                 }
             }
