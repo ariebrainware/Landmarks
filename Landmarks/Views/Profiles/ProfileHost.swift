05b1a14eba7c3f46c47cc8dffef18edc729b9c22
Arie Brainware <publicu001@protonmail.com>
2023-08-18 02:52:05 +0700

    Display a User Profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Arie Brainware on 18/08/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
