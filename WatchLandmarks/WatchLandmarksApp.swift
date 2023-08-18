999e3d0b8158395a95d0965f0f2b64d156dc3d72
Arie Brainware <publicu001@protonmail.com>
2023-08-19 03:47:14 +0700

    Add a watchOS Target
    and Share Files Between Targets

---
+++ WatchLandmarks/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks
+//
+//  Created by Arie Brainware on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
