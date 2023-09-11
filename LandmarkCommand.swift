fd299b98be44e1c7647c3881fb7d5cb97da4e488
Arie Brainware <publicu001@protonmail.com>
2023-09-11 11:15:05 +0700

    Add a Build-in Menu Command

---
+++ LandmarkCommand.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommand.swift
+//  Landmarks
+//
+//  Created by Arie Brainware on 11/09/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
